9776f5e2f8cb137ff08b9d231999b161b7ff8f5b
yveronne <veronneyepmo@gmail.com>
2023-10-03 09:48:30 -0400

    notebook for having switch of identities

--- Bytetrack/forwardBackward.py
+++ Bytetrack/forwardBackward.py
@@ -10,27 +10,25 @@ import copy
 #!pip install opencv-python
 import cv2
 POWERFULNESS=2
+hungarian = False
 Home_folder=  "/home/sophie/uncertain-identity-aware-tracking/Bytetrack"
+#en supposant que les observations sont independantes la normalisation à 1 des alpha et beta est acceptable 
 #la solution qui suivra sera de choisir l'identité la plus acceptée au niveau de L et de l'affecté à la localisation identifié dans le tracking 
-################proposition d'amélioration##################################
-#à ajouter: donner du poid aux éléments qui ont les même track_id ???? 
 
-######################################################################
+
 def process_forwad_backward(track_with_observation,nbr_visit="", json_save_path="/home/sophie/uncertain-identity-aware-tracking/Bytetrack/videos/GR77_20200512_111314_with_atq_tracking_with_HMM_result.json", video_path="/home/sophie/uncertain-identity-aware-tracking/Bytetrack/videos/GR77_20200512_111314.mp4"):
-    confidence_threshold = 0.3#1  #### sophie mod 
-    hungarian= False
+    confidence_threshold = 0.3  #### sophie mod 
     confidence_on_hmm_choice=2#1.5
     """_summary_
     parameter: confidence_threshold
 
     Returns:
-        write a video with ATQ and put the results in the file /home/sophie/uncertain-identity-aware-tracking/Bytetrack/videos/GR77_20200512_111314_with_atq_tracking_with_HMM_reslut.json
+        write a video with ATQ and put the results in the file /home/sophie/uncertain-identity-aware-tracking/Bytetrack/videos/GR77_20200512_111314_with_atq_tracking_with_HMM_resut.json
     """
     import numpy as np 
     def softmax(x):
         #return x/sum(x)
         #return np.exp(x)/sum(np.exp(x))
-
         return np.power(x,confidence_on_hmm_choice) / np.sum(np.power(x,confidence_on_hmm_choice), axis=0)
         
     def hungarian_choice(matrice,  value_of_confidence_on_track=1):
@@ -40,17 +38,6 @@ def process_forwad_backward(track_with_observation,nbr_visit="", json_save_path=
         return matrice
 
     def forward(V=np.array([0,1]), a={"t=1":np.array([[0.5,0.5]]) }, b={"t=0":np.array([0.5]),"t=1":np.array([0.5,0.5])}, initial_distribution=np.array([0.5]), T=1 ): #V=np.array([0,1]), a={"t=1":np.array([[0.5,0.5],[0.5,0.5]]) }, b={"t=0":np.array([0.5,0.5]),"t=1":np.array([0.5,0.5])}, initial_distribution=np.array([0.5,0.5])):
-        """
-
-        Args:
-            V (_type_, optional): _description_. Defaults to np.array([0,1]), a={"t=1":np.array([[0.5,0.5]]) }, b={"t=0":np.array([0.5]),"t=1":np.array([0.5,0.5])}, initial_distribution=np.array([0.5]), T=1 ):#V=np.array([0,1]).
-            a (dict, optional): _description_. Defaults to {"t=1":np.array([[0.5,0.5],[0.5,0.5]]) }.
-            b (dict, optional): _description_. Defaults to {"t=0":np.array([0.5,0.5]),"t=1":np.array([0.5,0.5])}.
-            initial_distribution (_type_, optional): _description_. Defaults to np.array([0.5,0.5]).
-
-        Returns:
-            _type_: _description_
-        """
         alpha = {}
         alpha[V[1]] = initial_distribution * b["t="+str(V[1])]
         for t in range(2, V.shape[0]):
@@ -70,9 +57,11 @@ def process_forwad_backward(track_with_observation,nbr_visit="", json_save_path=
                 alpha[V[t]]=np.zeros(a["t="+str(V[t])].shape[1])
                 for j in range(a["t="+str(V[t])].shape[1]):
                     alpha[V[t]][j] = tmp_alpha.dot(a["t="+str(V[t])][:, j]) *(b["t="+str(V[t])][j])
+                final_alpha_t=alpha[V[t]]
                 
+            ##print("*****",V[t], alpha[V[t]])
             if alpha[V[t]].sum()!=0:
-                alpha[V[t]]=softmax(alpha[V[t]] )
+                alpha[V[t]]=softmax(alpha[V[t]])
             
         return alpha
         
@@ -86,9 +75,9 @@ def process_forwad_backward(track_with_observation,nbr_visit="", json_save_path=
         for t in range(V.shape[0] - 2, -1, -1):
             #tmp_beta=beta[V[t+1]]
             #if "t="+str(V[t]) in list(b.keys()) and (b["t="+str(V[t])].max()==  b["t="+str(V[t])].min()) and  ( beta[V[t+1]].max()== beta[V[t+1]].min()):
-            #  ca c'est lorsqu'on a pas d'observation, et que l'on vient de commencer avec beta, on préfère 
-            # laisser beta t1+1 à 1 et ne pas se laisser influencer par les probabilités de bytetrack
-            # ??? mais je ne suis pas sure que ca influence par ce que a est normaliser à 1 comme somme de cells par ligne??? à veirifier et tester """
+            """ ca c'est lorsqu'on a pas d'observation, et que l'on vient de commencer avec beta, on préfère 
+            laisser beta t1+1 à 1 et ne pas se laisser influencer par les probabilités de bytetrack
+            ??? mais je ne suis pas sure que ca influence par ce que a est normaliser à 1 comme somme de cells par ligne??? à veirifier et tester """
             #  a_temp=np.ones(a["t="+str(V[t+1])].shape)
             #beta[V[t]]=beta[V[t+1]]
             #  beta[V[t]]=np.zeros(a["t="+str(V[t+1])].shape[0])
@@ -114,10 +103,18 @@ def process_forwad_backward(track_with_observation,nbr_visit="", json_save_path=
                         beta[V[t]][j] = (tmp_beta * b["t="+str(V[t + 1])]).dot(a["t="+str(V[t+1])][j, :])
                 if beta[V[t]].sum()!=0:
                     beta[V[t]]=softmax(beta[V[t]])
-                    
                 ##print("*****",V[t], beta[V[t]])
-                
-                
+            
+            """if "t="+str(V[t]) in list(b.keys()) :#and  b["t="+str(V[t])].max()!=  b["t="+str(V[t])].min():
+                #si on a une observation on ramène le compteur de beta à 0 avec 1 partout à la qui suivais frame
+                #tmp_beta= np.ones(beta[V[t+1]].shape)
+                tmp_beta=beta[V[t+1]]
+                beta[V[t]]=np.zeros(a["t="+str(V[t+1])].shape[0])
+                for j in range(a["t="+str(V[t+1])].shape[0]):
+                    beta[V[t]][j] = (tmp_beta * b["t="+str(V[t + 1])]).dot(a["t="+str(V[t+1])][j, :])
+
+                beta[V[t]]=softmax(beta[V[t]])#sophie mod  beta[V[t]]/beta[V[t]].sum() #"""
+
 
         return beta
     
@@ -128,25 +125,15 @@ def process_forwad_backward(track_with_observation,nbr_visit="", json_save_path=
         alpha = forward(V,a,b,initial_distribution,T)
         L={}
         for t in V[1:]:
+            #if t==400:
             #  #print(alpha[t],"***",beta[t])
-            alpha_tmp =alpha[t]
-            beta_tmp = beta[t]
-            L["t="+str(t)]=alpha[t]#*beta[t] + alpha[t] + beta[t] #alpha[t]*beta[t]
+            L["t="+str(t)]=alpha[t]*beta[t] + alpha[t] + beta[t] #alpha[t]*beta[t]
             if L["t="+str(t)].sum()!=0:
                 L["t="+str(t)]=L["t="+str(t)]/L["t="+str(t)].sum()
             L["t="+str(t)] = L["t="+str(t)].tolist()
-            """if t>=12080 and t<=12098:
-                    print(t, a["t="+str(t)].shape)
-                    infos=data[str(t)]["current"]
-                    a_tmp = a["t="+str(t)]
-                    L_tmp = L["t="+str(t)]
-                    beta_tmp = beta[t]
-                    alpha_tmp= alpha[t]
-                    print("ok")"""
         ##print("final t", t)
             
         return L,beta,alpha
-
     
 
     with open(track_with_observation) as f:
@@ -240,12 +227,13 @@ def process_forwad_backward(track_with_observation,nbr_visit="", json_save_path=
             all_known_track = [i for i in range(1,16,1) ]
             for identity in identities_list:
                 for idx, track in enumerate(data[str(t)]["current"]):
-                    proba[identity][track["track_id"]].append(int(L[identity]["t="+str(t)][idx] ))
+                    proba[identity][track["track_id"]].append(float(L[identity]["t="+str(t)][idx] ))
                     if track["track_id"] in all_known_track:
                         all_known_track.remove(track["track_id"])
                 if all_known_track!=[]:
                     for remaining_track in all_known_track:
                         proba[identity][remaining_track].append(None)
+        
         with open("proba.json", 'w') as outfile:
             json.dump(proba, outfile, cls=NumpyEncoder)
         print("proba dumped")
@@ -491,7 +479,7 @@ def process_forwad_backward(track_with_observation,nbr_visit="", json_save_path=
                         if track["atq_from_previous"] is not None:
                             #print("previous", track["atq_from_previous"] )
                             atq= track["atq_from_previous"]#+'fp'
-                            tid=tid+", atq:"+str(atq)+"("+str(round(L[track["atq_from_previous"]]["t="+str(frame_id)][idx], 2))+")"
+                            tid=tid+", atq:"+str(atq)+"(fp)"#+str(round(L[track["atq_from_previous"]]["t="+str(frame_id)][idx], 2))+")"
                         """elif track["atq_from_future"] is not None:
                             #print("future",track["atq_from_future"] )
                             atq= track["atq_from_future"]#+'ff'#"""
